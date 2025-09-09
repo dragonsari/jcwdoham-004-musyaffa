0318e1e9d4170e3eedde62c5497697bea1237efa
Musyaffa <naufal.musyaffa16@gmail.com>
2025-09-09 22:54:45 +0800

    letter "A" star and palindrome done

--- module-01/exercise-03/aBecomeStar.js
+++ module-01/exercise-03/aBecomeStar.js
@@ -1,1 +1,11 @@
 //change "a" letter to * (star)
+let sentence="An apple a day keeps the doctor away";
+let newSentence="";
+for(let i=0;i<sentence.length;i++){
+    if(sentence[i] >= 'A' && sentence[i] <='a'){
+        newSentence += "*";
+    }else{
+        newSentence+=sentence[i];
+    }
+}
+console.log(newSentence)
--- module-01/exercise-03/palindrome.js
+++ module-01/exercise-03/palindrome.js
@@ -1,1 +1,12 @@
 //A palindrome is a word, phrase, number, or other sequence of characters that reads the same forwards and backwards. The characters are mirrored around the center.
+let sentence="madam";
+let sentenceLowerCase=sentence.toLowerCase();
+for(let i=0;i<sentence.length/2;i++){
+    let leftWord=sentenceLowerCase[i];
+    let righWord=sentenceLowerCase[sentence.length-1-i];
+    if(leftWord!==righWord){
+        console.log("Not Palindrome");
+        break;
+    }
+    console.log("palindrome");
+}
